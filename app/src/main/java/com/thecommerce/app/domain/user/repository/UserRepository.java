4e9022b57b3fed9f076d16928dc323d8ff88f3f6
rnjsgo <rnjsgo2008@naver.com>
2024-04-26 21:43:52 +0900

    feat : create repository and unit test related to get users api
    - 정렬 확인을 위해 sql 스크립트 수정

--- app/src/main/java/com/thecommerce/app/domain/user/repository/UserRepository.java
+++ app/src/main/java/com/thecommerce/app/domain/user/repository/UserRepository.java
@@ -2,9 +2,15 @@ package com.thecommerce.app.domain.user.repository;
 
 import com.thecommerce.app.domain.user.entity.User;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUserId(String userId);
+
     Optional<User> findByNickname(String nickname);
+
+    Page<User> findAll(Pageable pageable);
 }
