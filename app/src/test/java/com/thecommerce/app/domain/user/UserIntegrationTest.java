c3ef0afc7b52358eddd1bbaeecea141769a0bf30
rnjsgo <rnjsgo2008@naver.com>
2024-04-27 14:54:02 +0900

    test : create an integration test related to get users api

--- app/src/test/java/com/thecommerce/app/domain/user/UserIntegrationTest.java
+++ app/src/test/java/com/thecommerce/app/domain/user/UserIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.thecommerce.app.domain.user;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
@@ -27,6 +30,7 @@ public class UserIntegrationTest {
     // ("api/user/join")
     @Test
     public void 회원가입() throws Exception {
+
         UserSignUpDto userSignUpDto = UserSignUpDto.builder()
                 .userId("newUser")
                 .password("password")
@@ -36,12 +40,26 @@ public class UserIntegrationTest {
                 .email("user01@example.com")
                 .build();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-
         mockMvc.perform(post("/api/user/join")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(userSignUpDto)))
+                        .content(om.writeValueAsString(userSignUpDto)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.success").value(true));
+    }
+
+    // ("api/user/list")
+    @Test
+    @Sql("classpath:sql/UserDummyData.sql")
+    public void 회원목록조회() throws Exception {
+
+        mockMvc.perform(get("/api/user/list")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .param("sort", "name,desc"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.response.users").isArray())
+                .andExpect(jsonPath("$.response.users[0].name").isNotEmpty())
+                .andDo(print());
     }
 }
